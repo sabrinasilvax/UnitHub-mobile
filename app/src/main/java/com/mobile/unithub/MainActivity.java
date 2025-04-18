8ce94adc960802f4891f985481d9cdb7584db8f0
Sabrina Guilherme <143810738+sabrinasilvax@users.noreply.github.com>
2025-04-18 20:47:18 -0300

    teste

--- app/src/main/java/com/mobile/unithub/MainActivity.java
+++ app/src/main/java/com/mobile/unithub/MainActivity.java
@@ -61,8 +61,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onSupportNavigateUp() {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        Log.i("first commit", "first commit");
-        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
+               return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
     }
 
